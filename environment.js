8ec589485dcbfd3ec0eb27ba6bcf1bed9fe47bba
Henrique Rangel <hrangel@nucleo235.com.br>
2016-06-09 18:07:54 -0300

    Versão Inicial/Setup

--- environment.js
+++ environment.js
@@ -1,22 +1,25 @@
 module.exports = {
-    
+    tokens: [],
+    tokensHash: [],
+    usersHash: [],
     loadSocketIo: function loadSocketIo(redis) {
+        var current_module = this;
+        var express = require('express');
+        var socketIO = require('socket.io');
+        var path = require('path');
 
         var port = process.env.PORT || 5001;
         if (process.env.NODE_ENV != 'production') {
             port = 5001; // run on a different port when in non-production mode.
         }
 
-        console.log('STARTING ON PORT: ' + port);
+        var server = express()
+          .use(function(req, res){ res.sendFile(INDEX) })
+          .listen(port, function() {console.log('Listening on: ' + port)});
 
-        var io = require('socket.io').listen(Number(port));
+        var io = socketIO(server);
 
         io.on('connection', function(socket) {
-
-            socket.on('realtime_user_id_connected', function(message) {
-                console.log('Realtime User ID connected: ' + message.userId);
-            });
-
             socket.on('disconnect',function(){
                 redis.sub.removeListener('message', onMessage); 
             });
@@ -29,19 +32,22 @@ module.exports = {
                     return;
                 }
 
-                msg = JSON.parse(message);
+                var data = JSON.parse(message);
+                if (data.hasOwnProperty('type') && data.type == 'realtime_message') {
+                    var msg = data;
 
-                var currentSocketIoUserId = socket.request.session['user_id'];
+                    var currentSocketIoUserId = socket.request.session['user_id'];
 
-                // if the recipient user id list is not part of the message
-                // then define it anyways.
-                if (msg.recipient_user_ids === undefined || msg.recipient_user_ids == null) {
-                    msg.recipient_user_ids = [];
-                }
+                    // if the recipient user id list is not part of the message
+                    // then define it anyways.
+                    if (msg.recipient_user_ids === undefined || msg.recipient_user_ids == null) {
+                        msg.recipient_user_ids = [];
+                    }
 
-                if (msg.recipient_user_ids.indexOf(currentSocketIoUserId) != -1) {
-                    delete msg.recipient_user_ids; //don't include this with the message
-                    socket.emit('realtime_msg', msg);
+                    if (msg.recipient_user_ids.indexOf(currentSocketIoUserId) != -1) {
+                        delete msg.recipient_user_ids; //don't include this with the message
+                        socket.emit('realtime_msg', msg);
+                    }
                 }
             };
 
@@ -51,8 +57,8 @@ module.exports = {
     },
 
     authorize: function authorize(io, redis) {
+        var current_module = this;
         io.use(function(socket, next) {
-           
             var sessionId = null;
             var userId = null;
 
@@ -70,53 +76,82 @@ module.exports = {
                 }
             }
 
-            sessionId = params["_rtToken"];
-            userId = params["_rtUserId"];
- 
-            // retrieve session from redis using the unique key stored in cookies
-            redis.getSet.hget([("rtSession-" + userId), sessionId], 
-                function(err, session) {
-                    if (err || !session) {
-                        next(new Error('Unauthorized Realtime user (session)'));
-                    } else {
-                        socket.request.session = JSON.parse(session);
-                        next();
-                    }
+            // autenticar sem REDIS
+            if (socket.request.session == null) {
+                // console.log(socket.request);
+                if (current_module.tokensHash.hasOwnProperty(params['access-token']))
+                    next()
+                else {
+                    console.log(current_module.tokensHash);
+                    console.log('Unauthorized JS user (session)');
+                    next(new Error('Unauthorized JS user (session)'));
                 }
-            );
-            
+
+            } else {
+                next();
+            }
         });
     },
 
     loadRedis: function loadRedis() {
+        var current_module = this;
         var redis = require('redis');
         var url = require('url');
-        var redisURL = url.parse("redis://127.0.0.1:6379/0");
+        
         var redisSub, redisPub, redisGetSet = null;
         
-        if (process.env.REDISCLOUD_URL == null) {
-            // use local client if there's no redis cloud url set up.
-            redisSub = redis.createClient();
-            redisPub = redis.createClient();
-            redisGetSet = redis.createClient();
-        } else {
-            // use environment redis connection info.
-            redisURL = url.parse(process.env.REDISCLOUD_URL);
-            redisSub = redis.createClient(redisURL.port, redisURL.hostname, {
-                no_ready_check: true
-            });
-            redisPub = redis.createClient(redisURL.port, redisURL.hostname, {
-                no_ready_check: true
-            });
-            redisGetSet = redis.createClient(redisURL.port, redisURL.hostname, {
-                no_ready_check: true
-            });
+        var redisURL;
+        if (process.env.REDISTOGO_URL == null)
+            redisURL = url.parse("redis://127.0.0.1:6379/0");
+        else    
+            redisURL = url.parse(process.env.REDISTOGO_URL);
+        
+        // console.log(redisURL);
+        redisSub = redis.createClient(redisURL.port, redisURL.hostname, {
+            no_ready_check: true
+        });
+        redisPub = redis.createClient(redisURL.port, redisURL.hostname, {
+            no_ready_check: true
+        });
+        redisGetSet = redis.createClient(redisURL.port, redisURL.hostname, {
+            no_ready_check: true
+        });
+        if (redisURL.auth != null) {
             redisSub.auth(redisURL.auth.split(":")[1]);
             redisPub.auth(redisURL.auth.split(":")[1]);
             redisGetSet.auth(redisURL.auth.split(":")[1]);
         }
+        // redisSub.on("subscribe", function (channel, count) {
+        //     console.log("a nice channel - I am sending a message.");
+        //     console.log("a nice channel - I am sending a second message.");
+        //     console.log("a nice channel - I am sending my last message.");
+        //     console.log(channel);
+        // });
+
+        redisSub.on("message", function (channel, message) {
+            if (channel == 'refresh_token') {
+                var data = JSON.parse(message);
+
+                // removes old if exists
+                var userKey = '' + data.user;
+                if (current_module.usersHash.hasOwnProperty(userKey)) {
+                    var currentToken = current_module.usersHash[userKey];
+                    current_module.tokens.splice(currentToken.tokenIndex, 1);
+                    
+                    if (current_module.tokensHash.hasOwnProperty(currentToken.token))
+                        delete current_module.tokensHash[currentToken.token];
+                }
+
+                current_module.tokens.push(data);
+                data.tokenIndex = current_module.tokens.length;
+
+                current_module.tokensHash[data.token] = data;
+                current_module.usersHash[userKey] = data;
+            }
+        });
 
         redisSub.subscribe('realtime_msg');
+        redisSub.subscribe('refresh_token');
 
         return {
             pub: redisPub,
